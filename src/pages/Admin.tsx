f7662a00662502f843a74f0e2ee96c95d11170c7
wayneye <mouthbombya@gmail.com>
2026-06-27 03:25:04 +0800

    修復 Admin.tsx import 錯誤：補上 react

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "";
+import { useEffect, useState } from "react";
 import Layout from "@/components/Layout";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
